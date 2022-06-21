41a596db5c79cff39e69ae4007c1bfc6610262ae
GiacomoDiNardo <vest_95@hotmail.it>
2022-06-21 18:47:25 +0200

    add header and footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,9 +1,110 @@
 <template>
-    <div>contenuto footer</div>
+    <div class="footer-bg py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <ul class="d-flex flex-wrap list-unstyled gap-5">
+                        <li class="m-height">
+                            <h3>DC Comics</h3>
+                            <ul class="list-unstyled">
+                                <li v-for="link in DcComicsLinks" :key="link">
+                                    <a href="#">{{ link }}</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="m-height">
+                            <h3>DC</h3>
+                            <ul class="list-unstyled">
+                                <li v-for="link in DcLinks" :key="link">
+                                    <a href="#">{{ link }}</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="m-height">
+                            <h3>Sites</h3>
+                            <ul class="list-unstyled">
+                                <li v-for="link in sitesLinks" :key="link">
+                                    <a href="#">{{ link }}</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="m-height">
+                            <h3>Shop</h3>
+                            <ul class="list-unstyled">
+                                <li v-for="link in shopLinks" :key="link">
+                                    <a href="#">{{ link }}</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
         name: "TheFooter",
+        data () {
+            return {
+                DcComicsLinks : [
+                     "Characters",
+                     "Comics",
+                     "Movies",
+                     "TV",
+                     "Games",
+                     "Video",
+                     "News",
+                ],     
+                DcLinks : [
+                    "Terms Of Use",
+                    "Privacy policy (New)",
+                    "Ad Choices",
+                    "Advertising",
+                    "Jobs",
+                    "Subscriptions",
+                    "Talent Workshops",
+                    "CPSC Certificates",
+                    "Ratings",
+                    "Shop Help",
+                    "Contact Us",
+                ],
+                sitesLinks : [
+                    "DC",
+                    "MAD Magazine",
+                    "DC Kids",
+                    "DC Universe",
+                    "DC Power Visa",
+                ],
+                shopLinks : [
+                    "Shop DC",
+                    "Shop DC Collectibles",
+                ],
+            }
+        }
     };
 </script>
+
+<style>
+.footer-bg {
+    background-image: url("../../public/img/footer-bg.jpg");
+    background-size: cover;
+    color: white;
+}
+
+ul li h3 {
+    text-transform: uppercase;
+}
+
+ul li a {
+    display: inline-block;
+    text-decoration: none;
+    color: white;
+    margin-bottom: 3px;
+}
+
+.m-height {
+    max-height: 215px;
+}
+</style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,83 @@
 <template>
-    <div>
-        
+    <div class="container py-4">
+        <div class="row justify-content-between align-items-center">
+            <div class="col header-logo">
+                <img src="/img/dc-logo.png" alt="">
+            </div>
+            <div class="col">
+                <ul class="d-flex list-unstyled justify-content-end gap-4">
+                    <li v-for="(link, i) in headLinks" :key="i">
+                        <a class="head-link" :href="link.href">{{ link.name }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "TheHeader",
+        data() {
+            return {
+                headLinks: [
+                    {
+                        name: "Characters",
+                        href: "#",
+                    },
+                    {
+                        name: "Comics",
+                        href: "#",
+                    },
+                    {
+                        name: "Movies",
+                        href: "#",
+                    },
+                    {
+                        name: "TV",
+                        href: "#",
+                    },
+                    {
+                        name: "Games",
+                        href: "#",
+                    },
+                    {
+                        name: "Collectibles",
+                        href: "#",
+                    },
+                    {
+                        name: "Videos",
+                        href: "#",
+                    },
+                    {
+                        name: "Fans",
+                        href: "#",
+                    },
+                    {
+                        name: "News",
+                        href: "#",
+                    },
+                    {
+                        name: "Shop",
+                        href: "#",
+                    },
+                ]
+            }
+        }
     };
 </script>
+
+<style scoped>
+    .header-logo img {
+        width: 90px;
+        height: 90px;
+    }
+
+    ul li a {
+        color: black;
+        text-transform: uppercase;
+        text-decoration: none;
+        font-size: 14px;
+        font-weight: bold;
+    }
+</style>
