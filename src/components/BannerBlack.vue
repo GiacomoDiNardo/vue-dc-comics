2308671451a8b99001ddc157fd10def0a9e9fe90
GiacomoDiNardo <vest_95@hotmail.it>
2022-06-21 18:47:15 +0200

    add header and footer

--- src/components/BannerBlack.vue
+++ src/components/BannerBlack.vue
@@ -1,9 +1,25 @@
 <template>
-    <div>contenuto BannerBlack</div>
+    <div class="bg-black py-5">
+        <div class="container">
+            <h2 class="content-text"> --> Content goes here </h2>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
         name: "BannerBlack",
     };
+    
 </script>
+
+<style>
+    .bg-black {
+        background-color: black;
+        color: white;
+    }
+
+    .content-text {
+        font-weight: bold;
+    }
+</style>
