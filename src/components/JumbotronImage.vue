a9bd02a6134145a9bc44b83fc9de4389d29df5bc
GiacomoDiNardo <vest_95@hotmail.it>
2022-06-22 18:12:57 +0200

    add jumbotron img

---
+++ src/components/JumbotronImage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="jumbotron"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.jumbotron {
+    background-image: url(../../public/img/jumbotron.jpg);
+    height: 400px;
+    background-size: cover;
+}
+</style>
